ee9fb8793757e367d819306db046747306f7f029
Justin Kacherian <49377855+JustinKayy@users.noreply.github.com>
2022-04-03 20:52:26 -0300

    Add new form dropdown

--- loadForm.js
+++ loadForm.js
@@ -1,25 +1,42 @@
-function loadMainForm() {
+/*function doGet(){
 
-    const htmlServ = HtmlService.createTemplateFromFile("main");
-    const html = htmlServ.evaluate();
-    html.setWidth(875).setHeight(875);
-    const ui = SpreadsheetApp.getUi();
-    ui.showModalDialog(html, "Edit Organization Data");
+  return HtmlService.createTemplateFromFile("main").evaluate();
 
-  }
+}*/
+
+function loadMainForm(){
+
+  const htmlServ = HtmlService.createTemplateFromFile("main");
+  const html = htmlServ.evaluate();
+  html.setWidth(875).setHeight(875);
+  const ui = SpreadsheetApp.getUi();
+  ui.showModalDialog(html, "Edit Organization Data");
+
+}
+
+function loadMainFormDonations(){
+
+  const htmlServ = HtmlService.createTemplateFromFile("mainDonations");
+  const html = htmlServ.evaluate();
+  html.setWidth(875).setHeight(875);
+  const ui = SpreadsheetApp.getUi();
+  ui.showModalDialog(html, "Edit Donation Data");
+
+}
 
 
-  function createMenu_() {
+function createMenu_(){
 
-    const ui = SpreadsheetApp.getUi();
-    const menu = ui.createMenu("Form");
-    menu.addItem("Open Form", "loadMainForm");
-    menu.addToUi();
+  const ui = SpreadsheetApp.getUi();
+  const menu = ui.createMenu("Form");
+  menu.addItem("Open Form", "loadMainForm");
+  menu.addItem("Donations Form", "loadMainFormDonations");
+  menu.addToUi();
 
-  }
+}
 
-  function onOpen() {
+function onOpen(){
 
-    createMenu_();
+  createMenu_();
 
-  }
+}
